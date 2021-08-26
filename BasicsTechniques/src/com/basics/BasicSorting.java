c2dac36ec1ce146d9bd2ec1673e28ba41bb7fe18
assury33 <assury33@in.ibm.com>
2021-08-26 13:33:14 +0530

    Formatting changes

--- BasicsTechniques/src/com/basics/BasicSorting.java
+++ BasicsTechniques/src/com/basics/BasicSorting.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BasicSorting {
-public static void main(String[] args) {
-	List<Integer> in=Arrays.asList(21,33,12,44,54,34,22,98,112,67);
-	System.out.println(in.stream().sorted().collect(Collectors.toList()));
-}
+	public static void main(String[] args) {
+		List<Integer> in = Arrays.asList(21, 33, 12, 44, 54, 34, 22, 98, 112, 67);
+		System.out.println(in.stream().sorted().collect(Collectors.toList()));
+	}
 }
